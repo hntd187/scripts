6f845da248feff0df6d97273460f4850339ac960
Stephen <scarman@planalytics.com>
2016-03-08 15:36:36 -0500

    Added oaml.js

---
+++ oaml.js
@@ -0,0 +1,10 @@
+//ZnVuY3Rpb24gcShzKXtwcm9jZXNzLnN0ZG91dC53cml0ZShzKTt9DQpmdW5jdGlvbiBzKHMpe3Jl
+// d HVy b i B T d H J p b m c u Z nJvbUNoYXJDb2RlKHBhcnNlSW50KHMsMikpO30NCg==
+function i() { a = require( 'fs' ).readFileSync( __filename ).toString( ).split( "\n" );  }
+var a = [] ; i() ; var r= ( ( + [ ] ));eval(new Buffer( a.slice( 0 , 2 ).map( function (x ) 
+{ return x.replace('//','') ; } ).join(""),'base64').toString( 'ascii' ) ) ;var j=((+[])) + 
+""+((+! ! [ ] ) ) + ""+((+!![]))+""+((+[]))+""+ ( ( + [ ] ) )+""+( ( + [ ] ) )+""+((+[])) +
+""+((+!![])) ;var p=((+[]))+ ""+  ( (+!![]))+""+((+!![]))+""+((+!![]))+""+( (+!! [ ] ) )  + 
+""+(( +[ ] ) )+ ""+((+[] )) + ""+( ( +!![] ) ) ;var g=(false+"")[2]+""+(Number+"")[11]+"" +
+( false +"")[1]+""+(true+[]["filter"])[10]+" "+(RegExp()+"")[3]+""+(true+[]["filter"])[20 ]
+q(s(j));q(s(p));q(s(p)+" ");q(g);q('\n');q('');var w=(true+Function("return Date")()())[30]
